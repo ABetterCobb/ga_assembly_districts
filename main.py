03a232428b97156f4da5f8c4a1cf2a4996c8de02
GuyLiner <samster@disroot.org>
2026-04-02 11:12:27 -0400

    Download all ga state districts

--- main.py
+++ main.py
@@ -1,15 +1,63 @@
-import xml.etree.ElementTree as ET
-import requests
+import boto3
+import os
+from botocore import UNSIGNED
+from botocore.config import Config
+import csv
+import geojson 
 
-# Listing all of the .geojson files in this bucvket that are associated with Georgia
-r = requests.get("https://gm-zdm.s3.amazonaws.com/?prefix=gj/pol/ga")
+s3 = boto3.client(
+    's3',
+    config=Config(signature_version=UNSIGNED)
+)
 
-tree = ET.fromstring(r.content)
+#Up to date state district information
+#https://data.openstates.org/people/current/ga.csv
 
-ns = {"s3": "http://s3.amazonaws.com/doc/2006-03-01/"}
+all_features = []
 
-for key in root.iter("{http://s3.amazonaws.com/doc/2006-03-01/}Key"):
-    if key.text.endswith(".geojson"):
-        print("https://s3.amazonaws.com/gm-zdm/" + key.text)
+bucket="gm-zdm"
+response = s3.list_objects_v2(Bucket=bucket,Prefix='gj/pol/ga')
 
-urls = []
+for obj in response.get('Contents', []):
+    key = obj['Key']
+    local_path = os.path.join('.', key)
+
+    # create directories if needed
+    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+
+    try:
+        s3.download_file(bucket, key, local_path)
+        
+        with open(local_path, 'r') as file:
+            district = geojson.load(file)
+
+        for feature in district["features"]:
+            district_type = feature["properties"]["zdm_id"]
+            district_number = feature["properties"]["pnm"]
+
+            with open("ga.csv", newline="") as f:
+                district_info = csv.DictReader(f)
+
+                if "rep" in district_type:
+                    for row in district_info:    
+                        if row["current_district"] == district_number and row["current_chamber"] == "lower":
+                            district['features'][0]['properties']['current_party'] = row["current_party"]
+                            district['features'][0]['properties']['current_chamber'] = row["current_chamber"]
+                            district['features'][0]['properties']['rep_name'] = row["name"]
+                            
+                if "sen" in district_type:
+                    for row in district_info:    
+                        if row["current_district"] == district_number and row["current_chamber"] == "upper":
+                            district['features'][0]['properties']['current_chamber'] = row["current_chamber"]
+                            district['features'][0]['properties']['current_party'] = row["current_party"]
+                            district['features'][0]['properties']['rep_name'] = row["name"]
+            
+            all_features.extend(district.get("features", []))                
+
+        combined = geojson.FeatureCollection(all_features)
+        with open('ga_assembly.geojson', 'w') as file:
+            geojson.dump(combined, file)
+
+        print(f"Downloaded {key}")
+    except Exception as e:
+        print(e)
